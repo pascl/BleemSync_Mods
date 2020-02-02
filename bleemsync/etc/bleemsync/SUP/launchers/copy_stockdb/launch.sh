bfb4e46f4fb79ff6b1cd250722a20cb3733c27ca
pascl <pascl1983@gmail.com>
2020-02-02 02:04:42 +0100

    remove unmodified (and unnecessary here) files from BS1.2 branch

--- bleemsync/etc/bleemsync/SUP/launchers/copy_stockdb/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-source "/var/volatile/bleemsync.cfg"
-
-echo 2 > /data/power/disable
-cp -f "/gaadata/databases/regional.db" "$bleemsync_path/etc/bleemsync/SYS/databases/stockRegional.db"
-echo 0 > /data/power/disable
